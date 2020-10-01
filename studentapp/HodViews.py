51cfdf27714614207c0b55ccd5e04504148fc4ca
sabbugiri <sabbugiri50@gmail.com>
2020-10-01 16:18:26 +0545

    managed staff details, subject details, student details

--- studentapp/HodViews.py
+++ studentapp/HodViews.py
@@ -84,3 +84,43 @@ def add_student_save(request):
         except:
             messages.error(request, "Failed to Add Student") 
             return HttpResponseRedirect("/add_student")   
+
+def add_subject(request):
+    courses = Courses.objects.all()
+    staffs = CustomUser.objects.filter(user_type=2)
+    return render(request, "hod_template/add_subject_template.html",{"staffs":staffs,"courses":courses})
+
+def add_subject_save(request):
+    if request.method != "POST":
+        return HttpResponse("Method not allowed")
+    else:
+        subject_name = request.POST.get("subject_name")
+        course_id = request.POST.get("course")
+        course = Courses.objects.get(id=course_id)
+        staff_id = request.POST.get("staff")
+        staff=CustomUser.objects.get(id=staff_id)
+
+        try:
+            subject = Subjects(subject_name=subject_name, course_id=course, staff_id=staff) 
+            subject.save() 
+            messages.success(request, "Successfully Added Student")
+            return HttpResponseRedirect("/add_subject")  
+        except:
+            messages.error(request, "Failed to Add Student") 
+            return HttpResponseRedirect("/add_subject")     
+
+def manage_staff(request):
+    staffs = Staffs.objects.all()
+    return render(request, "hod_template/manage_staff_template.html",{"staffs":staffs})
+
+def manage_student(request):
+    students = Students.objects.all()
+    return render(request, "hod_template/manage_student_template.html",{"students":students})
+
+def manage_course(request):
+    courses = Courses.objects.all()
+    return render(request, "hod_template/manage_course_template.html",{"courses":courses})
+
+def manage_subject(request):
+    subjects = Subjects.objects.all()
+    return render(request, "hod_template/manage_subject_template.html",{"subjects":subjects})
